40248b782f3c6681f5c77fc527d3bf83e9ccf549
Geziel Carvalho <geziel.natal@gmail.com>
2024-11-13 18:22:55 -0500

    14. Creating the DBContext Class

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using dotnet_razor_blog.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_razor_blog.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
