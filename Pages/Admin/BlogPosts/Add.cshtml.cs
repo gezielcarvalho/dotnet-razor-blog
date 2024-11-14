67c45c4f1823488f2219938c0ef2399ee9ec6639
Geziel Carvalho <geziel.natal@gmail.com>
2024-11-13 22:40:17 -0500

    21. Create Add BlogPost Razor Form and BindProperty

---
+++ Pages/Admin/BlogPosts/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet_razor_blog.Pages.Admin.BlogPosts
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
