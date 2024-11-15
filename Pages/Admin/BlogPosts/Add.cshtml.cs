39cb7efd2cc60985b76f413111d63432db950378
Geziel Carvalho <geziel.natal@gmail.com>
2024-11-15 11:21:26 -0500

    23. Create Blog Post (Save To Database)

--- Pages/Admin/BlogPosts/Add.cshtml.cs
+++ Pages/Admin/BlogPosts/Add.cshtml.cs
@@ -1,3 +1,5 @@
+using dotnet_razor_blog.Data;
+using dotnet_razor_blog.Models.Domain;
 using dotnet_razor_blog.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,12 +10,35 @@ namespace dotnet_razor_blog.Pages.Admin.BlogPosts
     {
         [BindProperty]
         public AddBlogPost AddBlogPostRequest { get; set; }
+
+        private readonly AppDbContext _context;
+        public AddModel(AppDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
         }
 
         public IActionResult OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _context.BlogPosts.Add(new BlogPost
+            {
+                Author = AddBlogPostRequest.Author,
+                Content = AddBlogPostRequest.Content,
+                FeaturedImageUrl = AddBlogPostRequest.FeaturedImageUrl,
+                Heading = AddBlogPostRequest.Heading,
+                PublishedDate = AddBlogPostRequest.PublishedDate,
+                PageTitle = AddBlogPostRequest.PageTitle,
+                ShortDescription = AddBlogPostRequest.ShortDescription,
+                UrlHandle = AddBlogPostRequest.UrlHandle,
+                Visible = AddBlogPostRequest.IsVisible
+            });
+            _context.SaveChanges();
             return RedirectToPage("Add");
         }
     }
